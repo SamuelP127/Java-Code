f36c16c306fbc628a14a88e2d694f1ae851ec1d9
SamuelP127 <144620512+SamuelP127@users.noreply.github.com>
2023-09-11 18:04:43 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,3 +5,17 @@ public class Main
         System.out.println("Hello, World!");
     }
 }
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+    double money = 455.37;
+    final boolean account = true;
+    int age = 16;
+
+    if(account){
+    System.out.println("You're " + age + ", and have this much money:" + money + " nice job keep it up!");
+    }
+    }
+}
