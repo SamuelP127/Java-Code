cb9f27b189b07de036c78b73c08e60c97a57ccfd
SamuelP127 <144620512+SamuelP127@users.noreply.github.com>
2023-10-19 17:42:34 -0400

    Update bank account.java

--- bank account.java
+++ bank account.java
@@ -24,11 +24,14 @@ class Account {
     public void withdraw(double amount) {
         balance -= amount;
     }
+
+    public String toString() {
+        return "Account{name='" + name + "', balance=" + balance + '}';
+    }
 }
 
-public class Bankaccount{
-    public static void main(String[]args)
-    {
+public class Bankaccount {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String account = "";
@@ -42,36 +45,35 @@ public class Bankaccount{
             }
         }
 
-        if(account.equals("Yes")){
+        if (account.equals("Yes")) {
 
             double min = 0;
             double max = 10000;
-            double money = (double)Math.floor(Math.random() * (max - min + 1) + min);
+            double money = (double) Math.floor(Math.random() * (max - min + 1) + min);
 
             System.out.println("Thanks for banking with us! Please enter your name: ");
             String name = scanner.nextLine();
 
             System.out.println("Hi! " + name + " you currently have $" + money);
-            
+
             String option = "";
             while (!option.equals("Deposit") && !option.equals("Withdrawal")) {
                 System.out.println("Would you like to make a deposit or withdrawal today? ");
                 option = scanner.nextLine().toUpperCase();
                 option = option.substring(0, 1).toUpperCase() + option.substring(1).toLowerCase();
 
-                if(!account.equals("Deposit") && !account.equals("Withdrawal")){
+                if (!account.equals("Deposit") && !account.equals("Withdrawal")) {
                     System.out.println("Invalid input, please enter 'Deposit' or 'Withdrawal'");
                 }
             }
 
-            if(option.equals("Deposit")){
+            if (option.equals("Deposit")) {
                 System.out.println("How much would you like to deposit today?");
                 double deposit = Double.parseDouble(scanner.nextLine());
                 money += deposit;
                 System.out.println("You now have $" + money);
                 System.out.println();
-            } 
-            else if (option.equals("Withdrawal")){ 
+            } else if (option.equals("Withdrawal")) {
                 System.out.println("How much would you like to withdraw today?");
                 double withdrawal = Double.parseDouble(scanner.nextLine());
                 money -= withdrawal;
@@ -79,25 +81,25 @@ public class Bankaccount{
                 System.out.println();
             }
             boolean deal;
-                if(money > 1000){
-                    deal = true;
-                    if(deal){
+            if (money > 1000) {
+                deal = true;
+                if (deal) {
                     System.out.println("Because you're such a valued customer here, we can offer you 2% interest rate compounded monthly!");
                     System.out.println("This means that next month you will have $" + money * 1.02);
                     System.out.println();
-                
+
                     System.out.println("Have a great day valued customer!");
-                    }
                 }
+            }
         }
-       
-        else if(account.equals("No")){
+
+        else if (account.equals("No")) {
             System.out.println("You don't have an account with us");
             System.out.println("Would you like to make an account (Yes or No)");
             String wantsIt = scanner.nextLine().toUpperCase();
             wantsIt = wantsIt.substring(0, 1).toUpperCase() + wantsIt.substring(1).toLowerCase();
-            
-            if(wantsIt.equals("Yes")){
+
+            if (wantsIt.equals("Yes")) {
                 System.out.println("Please enter your name: ");
                 String name = scanner.nextLine();
 
@@ -115,6 +117,8 @@ public class Bankaccount{
                 System.out.println("If you stay with us, next month you will have $" + newAccount.getBalance() * 1.03);
                 System.out.println();
                 System.out.println("Have a great day, valued customer!");
+
+                System.out.println("The length of the name is: " + newAccount.getName().length());
             } else {
                 System.out.println("Thank you for considering us. Have a great day!");
             }
