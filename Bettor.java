217a7d3e9867d9fa20f72cbffb72c02a1b3b2bf8
SamuelP127 <144620512+SamuelP127@users.noreply.github.com>
2023-11-17 10:06:36 -0500

    Update Bettor.java

--- Bettor.java
+++ Bettor.java
@@ -3,18 +3,34 @@ public class Bettor {
     Scanner s = new Scanner(System.in);
     private double balance;
     private double bet;
-    public double gamble(){
+    public double gamble() {
         System.out.println("How much do you want to bet? You have:$" + balance);
         bet = s.nextInt();
+        if (bet > balance) {
+            System.out.println("Error: Not enough money in account.");
+            String t = "";
+            while (!t.equals("Money") && !t.equals("Smaller")) {
+                System.out.println("Do you want to deposit more money or bet smaller? 'Money' or 'Smaller':");
+                t = s.nextLine();
+                if (t.equals("Money")) {
+                    System.out.println("How much do you want to deposit?");
+                    balance = s.nextInt();
+                    return balance;
+                }
+                if (t.equals("Smaller")) {
+                    System.out.println("How much do you want to bet? You have:$" + balance);
+                    bet = s.nextInt();
+                }
+            }
+        }
         return balance = balance - bet;
     }
-    public void displayBalance(){
-        System.out.println("You have $" + balance);
-    }
-    public double deposit(){
-        System.out.println("How much do you want to deposit?");
-        balance = s.nextInt();
-        return balance;
-    }
-
+        public void displayBalance(){
+            System.out.println("You have $" + balance);
+        }
+        public double deposit(){
+            System.out.println("How much do you want to deposit?");
+            balance = s.nextInt();
+            return balance;
+        }
 }
