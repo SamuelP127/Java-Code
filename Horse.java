636bf86beb3b24acdd7f8675ad96f482c5f38a36
SamuelP127 <144620512+SamuelP127@users.noreply.github.com>
2023-12-08 10:18:05 -0500

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 public class Main{
     public static void main(String[] args){
 
@@ -40,7 +41,9 @@ public class Main{
         names.add("Arrow");
         names.add("Kingston");
 
-        System.out.println(YELLOW_TEXT + "The selected horses for this race are..." + RESET);
+        String again = "";
+        //Going to add a while loop here so if the player loses or win they can play again easily.
+        System.out.print(YELLOW_TEXT + "The selected horses for this race are..." + RESET);
 
         ArrayList<String> horses = new ArrayList<String>();
         Random rand = new Random();
@@ -51,16 +54,17 @@ public class Main{
             horses.add(randomElement);
             System.out.print(randomElement + "! ");
         }
+        //need to fix this logic, sometimes will choose a horse twice
 
         Scanner s = new Scanner(System.in);
         Bettor b = new Bettor();
 
         String y = "";
         while(!y.equals("Yes") && !y.equals("No")){
-            System.out.print("Would you like to bet on this race? Yes or No:");
+            System.out.println("\nWould you like to bet on this race? Yes or No:");
             y = s.nextLine().toUpperCase();
             y = y.substring(0,1).toUpperCase() + y.substring(1).toLowerCase();
-            if(y.equals("Yes")){
+            if(y.equals("Yes")) {
                 b.displayBalance();
                 b.deposit();
                 b.gamble();
@@ -80,13 +84,19 @@ public class Main{
                     }
                 }
 
-                
-
-            } else if (y.equals("No")) {
+                int winner = rand.nextInt(horses.size());
+                String winner2 = horses.get(winner);
+                System.out.println("And the winner of this race is... " + winner2 + "!!!");
+                if (winner2.equals(horse)) System.out.println("Congratulations you won: $" + b.multiplier(b.getBet()));
+                else {
+                    System.out.println("So sorry, you lost: $" + b.getBet());
+                    System.out.println("Your new balance is: $" + b.newBalance()); //Not working correctly
+                }
+            }
+            else if (y.equals("No")) {
                 System.out.println("You're no fun");
             }
             else System.out.println("Invalid input");
         }
-
     }
 }
