6c5504921b0c82237c3b283605c12c8b81bc4122
SamuelP127 <144620512+SamuelP127@users.noreply.github.com>
2023-12-12 10:18:37 -0500

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -41,62 +41,72 @@ public class Main{
         names.add("Arrow");
         names.add("Kingston");
 
-        String again = "";
-        //Going to add a while loop here so if the player loses or win they can play again easily.
-        System.out.print(YELLOW_TEXT + "The selected horses for this race are..." + RESET);
-
-        ArrayList<String> horses = new ArrayList<String>();
-        Random rand = new Random();
-        int randNames = 3;
-        for (int i = 0; i < randNames; i++) {
-            int randomItem = rand.nextInt(names.size());
-            String randomElement = names.get(randomItem);
-            horses.add(randomElement);
-            System.out.print(randomElement + "! ");
-        }
-        //need to fix this logic, sometimes will choose a horse twice
-
-        Scanner s = new Scanner(System.in);
         Bettor b = new Bettor();
+        Scanner s = new Scanner(System.in);
+        String again = "";
 
-        String y = "";
-        while(!y.equals("Yes") && !y.equals("No")){
-            System.out.println("\nWould you like to bet on this race? Yes or No:");
-            y = s.nextLine().toUpperCase();
-            y = y.substring(0,1).toUpperCase() + y.substring(1).toLowerCase();
-            if(y.equals("Yes")) {
-                b.displayBalance();
-                b.deposit();
-                b.gamble();
-                String horse = "";
-                while (!horse.equals(horses.get(0)) && !horse.equals(horses.get(1)) && !horse.equals(horses.get(2))) {
-                    System.out.println("What horse would you like to bet on?");
-                    System.out.print(horses + "\n");
-                    horse = s.nextLine();
-                    if (horse.equals(horses.get(0))) {
-                        System.out.println("You bet $" + b.getBet() + " on " + horses.get(0));
-                    } else if (horse.equals(horses.get(1))) {
-                        System.out.println("You bet $" + b.getBet() + " on " + horses.get(1));
-                    } else if (horse.equals(horses.get(2))) {
-                        System.out.println("You bet $" + b.getBet() + " on " + horses.get(2));
-                    } else {
-                        System.out.println("Error, wrong input. Must be '" + horses.get(0) + "', '" + horses.get(1) + "', or '" + horses.get(2) + "'");
+        b.giveMoney();
+
+        while(b.getBalance() > 0){
+            System.out.println("Would you like to play:");
+            again = s.nextLine();
+            again = again.substring(0,1).toUpperCase() + again.substring(1);
+            if(again.equals("Yes")){
+                System.out.print(YELLOW_TEXT + "The selected horses for this race are..." + RESET);
+
+                ArrayList<String> horses = new ArrayList<String>();
+                Random rand = new Random();
+                int randNames = 3;
+                for (int i = 0; i < randNames; i++) {
+                    int randomItem = rand.nextInt(names.size());
+                    String randomElement = names.get(randomItem);
+                    horses.add(randomElement);
+                    System.out.print(randomElement + "! ");
+                }
+
+                //need to fix this logic, sometimes will choose a horse twice
+
+
+                String y = "";
+                while(!y.equals("Yes") && !y.equals("No")){
+                    System.out.println("\nWould you like to bet on this race? Yes or No:");
+                    y = s.nextLine().toUpperCase();
+                    y = y.substring(0,1).toUpperCase() + y.substring(1).toLowerCase();
+                    if(y.equals("Yes")) {
+                        System.out.println("Your balance is: " + b.getBalance());
+                        b.gamble();
+                        String horse = "";
+                        while (!horse.equals(horses.get(0)) && !horse.equals(horses.get(1)) && !horse.equals(horses.get(2))) {
+                            System.out.println("What horse would you like to bet on?");
+                            System.out.print(horses + "\n");
+                            horse = s.nextLine();
+                            if (horse.equals(horses.get(0))) {
+                                System.out.println("You bet $" + b.getBet() + " on " + horses.get(0));
+                            } else if (horse.equals(horses.get(1))) {
+                                System.out.println("You bet $" + b.getBet() + " on " + horses.get(1));
+                            } else if (horse.equals(horses.get(2))) {
+                                System.out.println("You bet $" + b.getBet() + " on " + horses.get(2));
+                            } else {
+                                System.out.println("Error, wrong input. Must be '" + horses.get(0) + "', '" + horses.get(1) + "', or '" + horses.get(2) + "'");
+                            }
+                        }
+
+                        int winner = rand.nextInt(horses.size());
+                        String winner2 = horses.get(winner);
+                        System.out.println("And the winner of this race is... " + winner2 + "!!!");
+                        if (winner2.equals(horse)) System.out.println("Congratulations you won: $" + b.multiplier(b.getBet()) + " Your new balance is: " + b.win());
+                        else {
+                            System.out.println("So sorry, you lost: $" + b.getBet());
+                            System.out.println("Your new balance is: $" + b.loss());
+                        }
                     }
-                }
-
-                int winner = rand.nextInt(horses.size());
-                String winner2 = horses.get(winner);
-                System.out.println("And the winner of this race is... " + winner2 + "!!!");
-                if (winner2.equals(horse)) System.out.println("Congratulations you won: $" + b.multiplier(b.getBet()));
-                else {
-                    System.out.println("So sorry, you lost: $" + b.getBet());
-                    System.out.println("Your new balance is: $" + b.newBalance()); //Not working correctly
+                    else if (y.equals("No")) {
+                        System.out.println("You're no fun");
+                    }
+                    else System.out.println("Invalid input");
                 }
             }
-            else if (y.equals("No")) {
-                System.out.println("You're no fun");
-            }
-            else System.out.println("Invalid input");
+            if(b.getBalance() == 0) System.out.print("You ran out of money :( game over!");
         }
     }
 }
